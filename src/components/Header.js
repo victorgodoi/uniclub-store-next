df30024012c88005b4ed6c11ec57f77b1a6b6451
Victor de Godoi Coelho <victordegodoi.c@gmail.com>
2023-11-29 17:58:06 -0300

    terminando o header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,33 +9,6 @@ import classNames from "classnames"
 import Link from "next/link"
 
 
-
-
-
-// const AccountMenu = styled.div`
-//     display: flex;
-//     gap: 55px;
-
-//     > div {
-//         position: relative;
-//     }
-// `
-
-// const Circle = styled.div`
-//     background-color: black;
-//     width: 17px;
-//     height: 17px;
-//     border-radius: 10px;
-//     color: white;
-//     font-size: 9px;
-//     display: flex;
-//     justify-content: center;
-//     align-items: center;
-//     position: absolute;
-//     top: -10px;
-//     left: 10px;
-// `
-
 // const Link = styled(NavLink)`
 //     text-decoration: none;
 //     color: #000;
@@ -54,6 +27,8 @@ import Link from "next/link"
 //     }
 // `
 
+
+
 const Component = () => {
 
     //const isMainRoute = useMatch("/");
@@ -66,13 +41,25 @@ const Component = () => {
                     <nav>
                         <ul className='flex gap-11'>
                             <Link className='no-underline text-black text-sm font-bold uppercase hover:underline hover:cursor-pointer' src='/'>home</Link>
-                            <Link className='no-underline text-black text-sm font-bold uppercase hover:underline hover:cursor-pointer' src='/'>home</Link>
-                            <Link className='no-underline text-black text-sm font-bold uppercase hover:underline hover:cursor-pointer' src='/'>home</Link>
-                            <Link className='no-underline text-black text-sm font-bold uppercase hover:underline hover:cursor-pointer' src='/'>home</Link>
-                            <Link className='no-underline text-black text-sm font-bold uppercase hover:underline hover:cursor-pointer' src='/'>home</Link>
-                            <Link className='no-underline text-black text-sm font-bold uppercase hover:underline hover:cursor-pointer' src='/'>home</Link>
+                            <Link className='no-underline text-black text-sm font-bold uppercase hover:underline hover:cursor-pointer' src='/'>shop</Link>
+                            <Link className='no-underline text-black text-sm font-bold uppercase hover:underline hover:cursor-pointer' src='/'>Hoodies</Link>
+                            <Link className='no-underline text-black text-sm font-bold uppercase hover:underline hover:cursor-pointer' src='/'>T-shirts</Link>
+                            <Link className='no-underline text-black text-sm font-bold uppercase hover:underline hover:cursor-pointer' src='/'>accessories</Link>
+                            <Link className='no-underline text-black text-sm font-bold uppercase hover:underline hover:cursor-pointer' src='/'>jackets</Link>
                         </ul>
                     </nav>
+                </div>
+                <div className='flex gap-14'>
+                    {/* <img></img> */}
+                    <div className='relative'>
+                        {/* <img></img> */}
+                        <div className='bg-black w-4 h-4 rounded-xl text-white text-[9px] flex justify-center items-center absolute t-[-10] l-[10]'>12</div>
+                    </div>
+                    <div classNames='relative'>
+                        {/* <img></img> */}
+                        <div className='bg-black w-4 h-4 rounded-xl text-white text-[9px] flex justify-center items-center absolute t-[-10] l-[10]'>3</div>
+                    </div>
+
                 </div>
             </div>
             {/* <Content>
